7de57e55747c09c7292868bbc663504abe50f1f1
jacoFixSmile <61232800+jacoFixSmile@users.noreply.github.com>
2021-02-11 11:44:41 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+<!DOCTYPEhtml>
+    <html>
+        <body>
+            <h2>SOPexample2</h2>
+            <iframe id="telenet"src="https://www.telenet.be">
+            </iframe>
+                <script>
+window.onload=function(){setTimeout(function(){document.getElementById('telenet').contentWindow.innerHTML;},5000)};
+                </script>
+            </body>
+</html>
